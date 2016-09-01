4d1132eecc496fcb7e91b05d39b9223961f07f17
RawIron <manianology@gmail.com>
2016-09-01 12:28:59 +0200

    use echo in test of Exec class

--- src/parser/ExecTest.java
+++ src/parser/ExecTest.java
@@ -2,16 +2,15 @@ package parser;
 
 import junit.framework.TestCase;
 import parser.Exec;
-import parser.P4Version;
 import parser.SystemCommand;
 
 
 public class ExecTest extends TestCase {
 
-    public final void test_runP4Version() {
-        SystemCommand p4Version = new P4Version();
+    public final void test_runEcho() {
+        Echo echo = new Echo();
         Exec exec = new Exec();
-        exec.execute(p4Version);
-        assertTrue(exec.stdout().startsWith("Perforce"));
+        exec.execute(echo);
+        assertTrue(exec.stdout().startsWith("does"));
     }
 }
